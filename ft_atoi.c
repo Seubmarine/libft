937b4ca04548beebf4a413b5deeb3936530a4183
Seubmarine <seubmarinepro@gmail.com>
2021-10-07 00:24:35 +0200

    remove ft_atoi main

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: tbousque <tbousque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/04 10:31:32 by tbousque          #+#    #+#             */
-/*   Updated: 2021/10/04 23:04:22 by tbousque         ###   ########.fr       */
+/*   Updated: 2021/10/06 22:05:40 by tbousque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -31,16 +31,4 @@ int	ft_atoi(const char *nptr)
 	while (*nptr && ft_isdigit(*nptr))
 		number = (number * 10) + (*nptr++ - 48);
 	return (number * sign);
-}
-
-#include<stdlib.h>
-int	main(int argc, char **argv)
-{
-	(void) argc;
-	(void) argv;
-	if (argc == 2)
-	{
-		printf("ft : %d\n", ft_atoi(argv[1]));
-		printf("std: %d\n", atoi(argv[1]));
-	}
 }
