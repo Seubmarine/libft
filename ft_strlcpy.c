9e787e0a594e25d770a34ebbde2159302e4de625
Seubmarine <seubmarinepro@gmail.com>
2021-10-11 06:52:36 +0200

    strlcpy return src len when size is 0

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -17,6 +17,8 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t i;
 
+	if (size == 0)
+		return (ft_strlen(src));
 	i = 0;
 	while (i < size - 1 && src[i] != '\0')
 		dst[i++] = src[i];
