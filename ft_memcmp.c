0e330f4c1f585b5a5a6513fd6735da17bba4c042
Seubmarine <seubmarinepro@gmail.com>
2021-10-15 01:15:15 +0200

    Added ft_memcmp.c

---
+++ ft_memcmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbousque <tbousque@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/11 22:55:53 by tbousque          #+#    #+#             */
+/*   Updated: 2021/10/15 01:13:37 by tbousque         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t				i;
+	const unsigned char	*str1;
+	const unsigned char	*str2;
+
+	i = 0;
+	str1 = s1;
+	str2 = s2;
+	while (str1[i] == str2[i] && i < n - 1)
+		i++;
+	return (str1[i] - str2[i]);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tbousque <tbousque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/13 23:46:59 by tbousque          #+#    #+#             */
-/*   Updated: 2021/10/11 12:27:36 by tbousque         ###   ########.fr       */
+/*   Updated: 2021/10/15 00:10:06 by tbousque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,5 @@ int ft_strncmp(const char *str1, const char *str2, size_t n);
 int	ft_atoi(const char *s);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+int ft_memcmp(const void *s1, const void *s2, size_t n);
 #endif
