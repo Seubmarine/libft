a4dbbc482b6fa8d2a8d3a9348c23fde32a8ae5d6
Seubmarine <seubmarinepro@gmail.com>
2021-10-17 18:57:50 +0200

    ft_strlcat added

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tbousque <tbousque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/13 23:46:59 by tbousque          #+#    #+#             */
-/*   Updated: 2021/10/15 00:10:06 by tbousque         ###   ########.fr       */
+/*   Updated: 2021/10/17 17:05:30 by tbousque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,13 @@ int	ft_isprint(int c);
 int	ft_toupper(int c);
 int	ft_tolower(int c);
 size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
-int ft_strncmp(const char *str1, const char *str2, size_t n);
+int	ft_strncmp(const char *str1, const char *str2, size_t n);
 int	ft_atoi(const char *s);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
-int ft_memcmp(const void *s1, const void *s2, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
 #endif
