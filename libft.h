7b20732daf4d28576756f6bf3bc9998d1eb7a190
Seubmarine <seubmarinepro@gmail.com>
2021-10-18 19:39:53 +0200

    forgot to update libft header

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tbousque <tbousque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/13 23:46:59 by tbousque          #+#    #+#             */
-/*   Updated: 2021/10/18 00:31:44 by tbousque         ###   ########.fr       */
+/*   Updated: 2021/10/18 00:52:30 by tbousque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,4 +36,9 @@ int	ft_atoi(const char *s);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
+void	ft_bzero(void *s, size_t n);
+
+#include <stdlib.h>
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strdup(const char *s);
 #endif
