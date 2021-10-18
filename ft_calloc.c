b6a3c3f73a76d889b4e6b7fca3b620eabd173778
Seubmarine <seubmarinepro@gmail.com>
2021-10-18 19:41:29 +0200

    finished part 1

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbousque <tbousque@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/18 00:34:29 by tbousque          #+#    #+#             */
+/*   Updated: 2021/10/18 00:45:51 by tbousque         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void *ft_calloc(size_t nmemb, size_t size)
+{
+	char *ptr;
+	
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (ptr);
+	ft_bzero(ptr, nmemb * size);
+	return (ptr);
+}
