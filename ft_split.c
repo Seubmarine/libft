736e80848f4f6163b740479e26c7b3aa17e72286
Seubmarine <seubmarinepro@gmail.com>
2021-10-28 15:56:23 +0200

    split work

--- ft_split.c
+++ ft_split.c
@@ -6,17 +6,17 @@
 /*   By: tbousque <tbousque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/21 17:24:14 by tbousque          #+#    #+#             */
-/*   Updated: 2021/10/28 15:01:24 by tbousque         ###   ########.fr       */
+/*   Updated: 2021/10/28 15:55:56 by tbousque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t ft_count_word(char const *s, char c)
+static size_t	ft_count_word(char const *s, char c)
 {
-	size_t wordcount;
-	size_t i;
-	int is_in_word;
+	size_t	wordcount;
+	size_t	i;
+	int		is_in_word;
 
 	wordcount = 0;
 	i = 0;
@@ -37,16 +37,18 @@ static size_t ft_count_word(char const *s, char c)
 	return (wordcount);
 }
 
-char **ft_split(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
-	char **words;
-	size_t start;
-	size_t end;
-	size_t word_i;
+	char	**words;
+	size_t	start;
+	size_t	end;
+	size_t	word_i;
 
 	if (s == NULL)
 		return (NULL);
-	words = malloc(sizeof(char *) * (ft_count_word(s, c) + 1));
+	words = (char **)malloc(sizeof(char *) * (ft_count_word(s, c) + 1));
+	if (words == NULL)
+		return (NULL);
 	end = 0;
 	word_i = 0;
 	words[word_i] = NULL;
@@ -58,20 +60,8 @@ char **ft_split(char const *s, char c)
 		end = start;
 		while (s[end] && s[end] != c)
 			end++;
-		char *word = ft_substr(s, start, end - start);
-		if (word == NULL)
-			return (NULL);
-		words[word_i++] = word;
+		words[word_i++] = ft_substr(s, start, end - start);
 		words[word_i] = NULL;
 	}
-	words[5] = NULL;
 	return (words);
 }
-/*
-int main(void)
-{
-     char *s = "      split       this for   me  !       ";
-     char **res = ft_split(s, ' ');
-     for (int i = 0; i <= 6; i++)
-         printf("%s", res[i]);
-}*/
