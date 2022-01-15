a254f5bdafd3692bb35c84d1463e8f2c7e3c87d9
Seubmarine <seubmarinepro@gmail.com>
2022-01-15 02:01:51 +0100

    rework itoa because of ullbase change

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: tbousque <tbousque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 02:39:38 by tbousque          #+#    #+#             */
-/*   Updated: 2021/10/30 22:48:47 by tbousque         ###   ########.fr       */
+/*   Updated: 2022/01/14 21:49:36 by tbousque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -14,9 +14,14 @@
 char	*ft_itoa(int n)
 {
 	char	*str;
+	char	*result;
 
-	str = ft_ullbase(ft_abs(n), "0123456789", 10, n < 0);
+	str = ft_ullbase(ft_abs(n), "0123456789", 10);
 	if (str == NULL)
+		return (NULL);
+	result = ft_strjoin("-", str);
+	free(str);
+	if (result == NULL)
 		return (NULL);
 	return (str);
 }
